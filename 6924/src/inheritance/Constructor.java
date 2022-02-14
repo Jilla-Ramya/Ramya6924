2a5a50d011bab8ee12c6514f504b9639be3f2ca2
ramya <ramya@LAPTOP-DEBTC9S9>
2022-02-14 10:09:16 +0530

    Constructor

--- 6924/src/inheritance/Constructor.java
+++ 6924/src/inheritance/Constructor.java
@@ -1,40 +1,34 @@
 package inheritance;
 
-public class Constructor 
+class Area 
 {
-		public Constructor (int a) 
-		//Area of square
-		{
-			int Constructor= a*a;
-			System.out.println("Side of square is "+a);
-			System.out.println("Area of square is "+Constructor); 
-		}
-		// Area of Rectangle
-		public Constructor (int l,int b) 
-		{
-			System.out.println("\n------------------------------------");
-			int Constructor  = l*b;
-			System.out.println("Length of Rectangle "+l);
-			System.out.println("Breadth of Rectangle "+b);
-			System.out.println("Area of Rectangle is "+Constructor); 
-		}
-		// Area of Circle
-		public Constructor (double pi, int r)
-		{
-			System.out.println("\n------------------------------------");
-			double Constructor  = pi*r*r;
-			System.out.println("Radius of Ciecle "+r);
-			System.out.println("Area of Circle is "+Constructor); 
-		}
-class ConstrcutorOverloading
-{
-	public void main(String args[]) 
+	
+	public Area(int a)
 	{
-		Constructor obj1 = new Constructor (10);
-		Constructor obj2 = new Constructor(101,20);
-		Constructor obj3 = new Constructor (3.14, 4);
+		int area= a*a;
+		System.out.println("Side of square is "+a);
+		System.out.println("Area of square is "+area); 
 	}
-		
+	public Area(int l,int b) 
+	{
+		int area = l*b;
+		System.out.println("Length of Rectangle "+l);
+		System.out.println("Breadth of Rectangle "+b);
+		System.out.println("Area of Rectangle is "+area); 
 	}
-
-}
+	public Area(double pi, int r) 
+	{
+		double area = pi*r*r;
+		System.out.println("Radius of Ciecle "+r);
+		System.out.println("Area of Circle is "+area); 
+	}
+	}
+	class ConstrcutorOverloading
+	{
+	public static void main(String args[])
+	{
+	Area obj1 = new Area(10);
+	Area obj2 = new Area(101,20);
+	Area obj3 = new Area(3.14, 4);
+	}
+	}
